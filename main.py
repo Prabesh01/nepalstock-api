fa50b622f51e5410ab8a04e26d5a9b6599c066ba
Prabesh Aryal <73214060+prabesharyal@users.noreply.github.com>
2023-08-05 09:06:26 +0545

    Spelling Error Fix
    A minor mistake but great confusion, the address printed in terminal was wrong, fixed.

--- main.py
+++ main.py
@@ -237,9 +237,9 @@ class S(BaseHTTPRequestHandler):
              
 
 port = int(os.environ.get("PORT", 5000))
-def run(server_class=HTTPServer, handler_class=S, addr="0.0.0.0", port=port):
+def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=port):
     server_address = (addr, port)
     httpd = server_class(server_address, handler_class)
-    print(f"Starting httpd server on {addr}:{port}")
+    print(f"Starting http server on http://{addr}:{port}")
     httpd.serve_forever()
 run()
